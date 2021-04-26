8e6753544085dc7d015fdb3d2e53b48c5b49fdd6
swathiacharya <acharyaswathi999@gmail.com>
2021-04-26 19:44:36 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  flag:boolean = true
+
+
+  apply(value:string){
+    this.flag = value == "login"?true : false;
+  }
 }
